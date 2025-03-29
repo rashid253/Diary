8fe95a55cb7579092d2910700447480489d94057
rashid253 <rashidncit@gmail.com>
2025-03-29 09:37:46 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,22 @@
 const cacheName = 'diary-app-cache-v1';
 const assetsToCache = [
-    './',
-    './index.html',
-    './style.css',
-    './script.js',
-    './manifest.json'
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(cacheName).then((cache) => cache.addAll(assetsToCache))
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(assetsToCache))
+  );
 });
 
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => response || fetch(event.request))
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
 });
